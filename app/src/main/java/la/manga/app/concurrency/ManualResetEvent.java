5bc844938b42bf1c74515e3f8acf382ccea56a25
Yam Marcovic <ymarcov@gmail.com>
2016-08-12 15:47:26 +0300

    Add ManualResetEvent

---
+++ app/src/main/java/la/manga/app/concurrency/ManualResetEvent.java
@@ -0,0 +1,35 @@
+package la.manga.app.concurrency;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * A manual reset event.
+ */
+public class ManualResetEvent {
+    private volatile boolean set = false;
+    private final Object syncObject = new Object();
+
+    public void signal() {
+        set = true;
+
+        synchronized (syncObject) {
+            syncObject.notifyAll();
+        }
+    }
+
+    public void waitForSignal() throws InterruptedException {
+        synchronized (syncObject) {
+            while (!set)
+                syncObject.wait();
+        }
+    }
+
+    public void waitForSignal(long millis) throws InterruptedException, TimeoutException {
+        synchronized (syncObject) {
+            syncObject.wait(millis);
+
+            if (!set)
+                throw new TimeoutException();
+        }
+    }
+}
---
+++ app/src/test/java/la/manga/app/concurrency/ManualResetEventTest.java
@@ -0,0 +1,91 @@
+package la.manga.app.concurrency;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class ManualResetEventTest {
+    private ManualResetEvent mre;
+
+    private Thread startThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.start();
+        return t;
+    }
+
+    @Before
+    public void setUp() {
+        mre = new ManualResetEvent();
+    }
+
+    @Test
+    public void signals() throws Exception {
+        startThread(new Runnable() {
+            @Override
+            public void run() {
+                mre.signal();
+            }
+        });
+
+        mre.waitForSignal();
+    }
+
+    @Test
+    public void waitsWithTimeout() throws Exception {
+        long expectedElapsed_ms = 100;
+        long expectedElapsed_ns = TimeUnit.MILLISECONDS.toNanos(expectedElapsed_ms);
+        boolean timeoutOccurred = false;
+
+        long start = System.nanoTime();
+
+        try {
+            mre.waitForSignal(expectedElapsed_ms);
+        } catch (TimeoutException _) {
+            timeoutOccurred = true;
+        }
+
+        long end = System.nanoTime();
+
+        assertTrue(timeoutOccurred);
+        assertThat(end - start, greaterThanOrEqualTo(expectedElapsed_ns));
+    }
+
+    @Test
+    public void returnsBeforeTimeout() throws Exception {
+        final long expectedElapsed_ms = 50;
+        long expectedElapsed_ns = TimeUnit.MILLISECONDS.toNanos(expectedElapsed_ms);
+        boolean timeoutOccurred = false;
+
+        long start = System.nanoTime();
+
+        try {
+            startThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(expectedElapsed_ms);
+                    } catch (Exception _) {
+                        // ignored
+                    }
+
+                    mre.signal();
+                }
+            });
+
+            mre.waitForSignal(expectedElapsed_ms * 2);
+        } catch (TimeoutException _) {
+            timeoutOccurred = true;
+        }
+
+        long end = System.nanoTime();
+
+        assertFalse(timeoutOccurred);
+        assertThat(end - start, greaterThanOrEqualTo(expectedElapsed_ns));
+        assertThat(end - start, lessThan(expectedElapsed_ns * 2));
+    }
+}
