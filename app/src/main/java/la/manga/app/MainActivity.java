3c33ee63455f7af70470f610c067b27d5a871c37
Yam Marcovic <ymarcov@gmail.com>
2016-08-17 09:23:38 +0300

    Add content area

--- app/src/main/java/la/manga/app/MainActivity.java
+++ app/src/main/java/la/manga/app/MainActivity.java
@@ -2,19 +2,37 @@ package la.manga.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, DownloadService.class));
+        handler = new Handler();
+        //startService(new Intent(this, DownloadService.class));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                TextView subtitle = (TextView) findViewById(R.id.subtitle);
+                subtitle.setVisibility(View.INVISIBLE);
+
+                RelativeLayout layout = (RelativeLayout) findViewById(R.id.contentArea);
+                layout.setVisibility(View.VISIBLE);
+            }
+        }, 1500);
     }
 
     @Override
